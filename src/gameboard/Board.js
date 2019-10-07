4db80fd62e99a96b61cb415d7da18ce91c55ba5e
Steven Senkowski <scsenkowski@gmail.com>
2019-10-07 10:42:59 -0400

    Moved axios getPieces get request to Board.js and passed the state 'Pieces' as props to <Piece/>. Mapped through pieces prop to create individual 'game-piece' cards.

--- src/gameboard/Board.js
+++ src/gameboard/Board.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import Piece from './Piece';
+import axios from 'axios';
+
 
 function createGame(e) {
   e.preventDefault();
@@ -7,16 +9,38 @@ function createGame(e) {
 }
 
 class Board extends Component {
-    render() {
-        return (
-        <Piece />,
+  constructor(props) {
+    super(props)
+    this.state = {
+      pieces: []
+    }
+  }
 
+  getPieces() {
+    axios.get('/api/v1/cards')
+    .then(response => {
+      this.setState({pieces: response.data})
+    })
+    .catch(error => console.log(error))
+  }
+
+  componentDidMount() {
+    this.getPieces()
+  }
+
+    render() {
+      console.log(this.state)
+        return (
+        <Piece pieces={this.state.pieces} />
+        /*Pass prop notaMatch or match! to <Piece/>.  if notaMatch
+        reset piece to isFlipped: false else  add points to scoreboard 
+          */
         /* Need to replicate <Piece/> to make a 5x5 game board
         with the different retrieved this.state.text results 
         from Piece.js get request generated when the play button
         is clicked.  They should be evenly spaced throughout the
         visible window */
-)
+      )
     };
 }
 
--- src/gameboard/Piece.js
+++ src/gameboard/Piece.js
@@ -5,35 +5,22 @@ function flipPiece(e) {
   e.preventDefault();
   e = document.querySelector('.game-piece')
   e.style.backgroundColor = "aliceblue";
+  
   }
 
 class Pieces extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      pieces: []
-    }
-  }
-
-  getPieces() {
-    axios.get('/api/v1/cards')
-    .then(response => {
-      this.setState({pieces: response.data})
-    })
-    .catch(error => console.log(error))
-  }
-
-  componentDidMount() {
-    this.getPieces()
-  }
-
   render() {
-    console.log(this.state)
+  
+    let pieces = this.props.pieces
+    
     return(
-  <div className="live-board">
-    <div onClick={flipPiece} className="game-piece" >
-     </div>
-  </div>)
+      <div className="live-board">
+        {pieces.map((piece) => 
+        <div key={piece.id} onClick={flipPiece} className="game-piece" >
+          <p> {piece.text} </p>
+         </div>
+         )}
+      </div>)
 };}
 
 export default Pieces;
